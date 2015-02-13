781f1cff4aa49d72cc91354a9fb7a1ae98f4f931
hjh399 <lilyhuang399@gmail.com>
2015-02-13 11:04:46 +0800

    E2_25跟书上写的有点不一样-判断数字大小进而修改程序的方式不同

---
+++ E2_25.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class E2_25 {
+  public static void main(String[] args) {
+	  
+	  
+	  int number1 = (int)(Math.random() * 10);
+	  int number2 = (int)(Math.random() * 10);
+	  int number = number1 - number2;
+	  
+	  if ( number1 < number2 )
+	  {
+		  System.out.println("what is " + number2 + " - " + number1 + " ?");
+		  number = -number;
+	  }
+	  else
+	  {
+		  System.out.println("what is " + number1 + " - " + number2 + " ?");
+	  }
+	  
+	  Scanner input = new Scanner(System.in);	  
+	  int answer = input.nextInt();
+	  
+	  if (answer == number)
+	  {
+		  System.out.println("答对了,是" + number);		  
+	  }
+	  else
+	  {
+		  System.out.println("答错了,应该是" + number );
+	  }
+  }
+}
+
+ 
